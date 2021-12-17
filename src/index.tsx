f1ed811cbef2fe8a93ff05add24b046023056b72
Erdoğan Bulut <e-bulut@hotmail.com.tr>
2021-12-17 22:02:39 +0300

    Add redux and redux toolkit support

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 
 import { BrowserRouter } from 'react-router-dom';
 
+import { Provider } from 'react-redux';
+import { store } from './store';
+
 import './styles/global.scss';
 
 import App from './App';
@@ -10,9 +13,11 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { useAppDispatch, useAppSelector } from '../../store/hooks';
+import { decrement, increment } from '../../store/slices/counter.slice';
 
 const Home = function () {
-  return <div className="Home">Home works!</div>;
+  const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
+  return (
+    <div className="Home">
+      <p>Home works!</p>
+      <div>
+        <button aria-label="Increment value" onClick={() => dispatch(increment())}>
+          Increment
+        </button>
+        <span>{count}</span>
+        <button aria-label="Decrement value" onClick={() => dispatch(decrement())}>
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
